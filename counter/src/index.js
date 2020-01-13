128d3b180a497da81ddcc7ed92437abc7b603238
Hellsingi <f.c.realmadrid@mail.ru>
2020-01-13 23:04:20 +0300

    After video 110 payload

--- counter/src/index.js
+++ counter/src/index.js
@@ -3,8 +3,14 @@ import { createStore } from 'redux';
 const reducer = (state = 0, action) => {
 
     switch (action.type) {
+        case 'RND':
+            return state + action.payload;
+
         case 'INC':
             return state + 1;
+
+        case 'DEC':
+            return state - 1;
 
         default:
             return state;
@@ -12,10 +18,33 @@ const reducer = (state = 0, action) => {
 };
 
 const store = createStore(reducer);
-store.subscribe(() => {
-    console.log(store.getState());
-});
 
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
+document
+    .getElementById('inc')
+    .addEventListener('click', () => {
+        store.dispatch({ type: 'INC' });
+    });
+
+document
+    .getElementById('dec')
+    .addEventListener('click', () => {
+        store.dispatch({ type: 'DEC' });
+    });
+
+document
+    .getElementById('rnd')
+    .addEventListener('click', () => {
+        const payload = Math.floor(Math.random() * 10);
+        store.dispatch({
+            type: 'RND',
+            payload
+        });
+    });
+
+const update = () => {
+    document.getElementById('counter')
+        .innerHTML = store.getState();
+}
+
+store.subscribe(update);
 
