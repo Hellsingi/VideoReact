bdcea4c940d93d97833898245f88e894d90d2bab
Hellsingi <f.c.realmadrid@mail.ru>
2020-01-20 00:16:17 +0300

    After video 131 "ownProps in mapDispatchToProps"

--- restore/src/components/book-list/book-list.js
+++ restore/src/components/book-list/book-list.js
@@ -3,29 +3,28 @@ import BookListItem from "../book-list-item";
 import { connect } from "react-redux";
 
 import { withBookstoreService } from "../hoc";
-import { booksLoaded, booksRequested } from "../../actions";
+import { booksLoaded, booksRequested, booksError } from "../../actions";
 import { compose } from "../../utils";
 import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
 import "./book-list.css";
 
 class BookList extends Component {
     componentDidMount() {
-        //   1. receive data
-        const { bookstoreService, booksLoaded, booksRequested } = this.props;
-        booksRequested();
-        bookstoreService.getBooks()
-            .then((data) => booksLoaded(data));
-
-        // 2. dispatch action to store
-        // this.props.booksLoaded(data);
+        this.props.fetchBooks();
     }
 
     render() {
-        const { books, loading } = this.props;
+        const { books, loading, error } = this.props;
+
+        if (error) {
+            return <ErrorIndicator />;
+        }
 
         if (loading) {
             return <Spinner />
         }
+
         return (
             <ul className="book-list">
                 {books.map(book => {
@@ -40,14 +39,21 @@ class BookList extends Component {
     }
 }
 
-const mapStateToProps = ({ books, loading }) => {
-    return { books, loading };
+const mapStateToProps = ({ books, loading, error }) => {
+    return { books, loading, error };
 };
 
-const mapDispatchToProps = {
-    booksLoaded,
-    booksRequested
-};
+const mapDispatchToProps = (dispatch, ownProps) => {
+    const { bookstoreService } = ownProps;
+    return {
+        fetchBooks: () => {
+            dispatch(booksRequested());
+            bookstoreService.getBooks()
+                .then((data) => dispatch(booksLoaded(data)))
+                .catch((err) => dispatch(booksError(err)));
+        }
+    }
+}
 
 export default compose(
     withBookstoreService(),
