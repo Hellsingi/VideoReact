7c2302b597a3a0e4399a56055014e712305fc921
Hellsingi <f.c.realmadrid@mail.ru>
2020-01-21 00:08:38 +0300

    After video 135 "add in array"

--- restore/src/reducers/index.js
+++ restore/src/reducers/index.js
@@ -1,53 +1,61 @@
+
 const initialState = {
     books: [],
     loading: true,
     error: null,
-    cartItems: [
-        {
-            id: 1,
-            name: 'Biik 1',
-            count: 3,
-            total: 150
-        },
-        {
-            id: 2,
-            name: 'Books 2',
-            count: 2,
-            total: 70
-        }
-    ],
-    orderTotal: 222
-};
-
-const reducer = (state = initialState, action) => {
+    cartItems: [],
+    orderTotal: 220
+  };
+  
+  const reducer = (state = initialState, action) => {
+  
     switch (action.type) {
-        case 'FETCH_BOOKS_REQUESTED':
-            return {
-                ...state,
-                books: [],
-                loading: false,
-                error: null
-            };
-
-        case 'FETCH_BOOKS_SUCCESS':
-            return {
-                ...state,
-                books: action.payload,
-                loading: false,
-                error: null
-            };
-
-        case 'FETCH_BOOKS_FAILURE':
-            return {
-                ...state,
-                books: [],
-                loading: false,
-                error: action.payload
-            }
-
-        default:
-            return state;
+      case 'FETCH_BOOKS_REQUEST':
+        return {
+          ...state,
+          books: [],
+          loading: true,
+          error: null
+        };
+  
+      case 'FETCH_BOOKS_SUCCESS':
+        return {
+          ...state,
+          books: action.payload,
+          loading: false,
+          error: null
+        };
+  
+      case 'FETCH_BOOKS_FAILURE':
+        return {
+          ...state,
+          books: [],
+          loading: false,
+          error: action.payload
+        };
+  
+      case 'BOOK_ADDED_TO_CART':
+        const bookId = action.payload;
+        const book = state.books.find((book) => book.id === bookId);
+        const newItem = {
+          id: book.id,
+          name: book.title,
+          count: 1,
+          total: book.price
+        };
+  
+        return {
+          ...state,
+          cartItems: [
+            ...state.cartItems,
+            newItem
+          ]
+        };
+  
+      default:
+        return state;
     }
-};
-
-export default reducer;
+  };
+  
+  export default reducer;
+  
