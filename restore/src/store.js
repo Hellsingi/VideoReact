6138ea3976b4a171a333f62da09c7be2a5f5d200
Hellsingi <f.c.realmadrid@mail.ru>
2020-01-26 21:23:03 +0300

    Add setting for redux DevTools

--- restore/src/store.js
+++ restore/src/store.js
@@ -1,7 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers =
+  process.env.NODE_ENV !== 'production' &&
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+/* eslint-enable */
+
+const store = createStore(reducer, composeEnhancers());
 
 export default store;
