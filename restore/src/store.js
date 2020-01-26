933376e94841dfc8f63a89a870e74faf61f2f8c2
Hellsingi <f.c.realmadrid@mail.ru>
2020-01-26 22:39:22 +0300

    After video 139 "store enhancer"

--- restore/src/store.js
+++ restore/src/store.js
@@ -4,12 +4,51 @@ import reducer from './reducers';
 
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers =
-  process.env.NODE_ENV !== 'production' &&
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+    process.env.NODE_ENV !== 'production' &&
+        typeof window === 'object' &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 /* eslint-enable */
 
-const store = createStore(reducer, composeEnhancers());
+
+
+const stringEnchancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+
+    const originalDispatch = store.dispatch;
+
+    store.dispatch = (action) => {
+        if (typeof action === 'string') {
+            return originalDispatch({
+                type: action
+            })
+        }
+
+        originalDispatch(action);
+    }
+
+    return store;
+};
+
+
+const logEnchancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+
+    const originalDispatch = store.dispatch;
+
+    store.dispatch = (action) => {
+        console.log(action.type);
+        originalDispatch(action);
+    }
+
+    return store;
+};
+
+
+
+
+const store = createStore(reducer, compose(stringEnchancer, logEnchancer));
+
+store.dispatch('Hello worls');
 
 export default store;
